122bd11cf9307179dfac4d6c296b3d85b9bc30ed
Ryan Bey <bey15002@byui.edu>
2022-03-10 23:06:34 -0700

    finished mobile homepage

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
-document.querySelector('#footer-year').innerText = new Date().getFullYear();
+// document.querySelector('#footer-year').innerText = new Date().getFullYear();
+
+// Add onclick for mobile buttons to animate a button press when tapped
