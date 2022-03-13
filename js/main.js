9f62240034509baaa9959997296d883a75fd356d
Ryan Bey <bey15002@byui.edu>
2022-03-12 18:23:39 -0700

    added todo notes

--- js/main.js
+++ js/main.js
@@ -11,3 +11,9 @@ buildMainMenu(jsonURL);
 
 // Footer copyright date
 document.querySelector('#footer-year').innerText = new Date().getFullYear();
+
+// NOTES!
+// Use multipage form to get matchup results
+// Populate first matchups based on json contents
+// Populate next matchups based on results of the previous matchup
+// https://www.w3schools.com/howto/howto_js_form_steps.asp
