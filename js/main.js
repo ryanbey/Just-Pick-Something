6043a50e8ca54d20e2c14bfd37a7f49dec3e9d83
Ryan Bey <bey15002@byui.edu>
2022-03-19 15:07:32 -0600

    json now using external url

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { buildMainMenu } from "./renderer.js";
 
-const jsonURL = '../json/brackets.json';
+const jsonURL = 'https://raw.githubusercontent.com/ryanbey/just-pick-something/main/json/brackets.json';
 
 buildMainMenu(jsonURL);
 
