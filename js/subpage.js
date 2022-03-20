13aaadbd969cda04d2c8c792e6636a604de8cff4
Ryan Bey <bey15002@byui.edu>
2022-03-19 18:02:01 -0600

    finished matchup css

--- js/subpage.js
+++ js/subpage.js
@@ -4,5 +4,9 @@ const jsonURL = '../json/brackets.json';
 
 // Build matchups
 
+
+// Testing
+
+
 // Footer copyright date
 document.querySelector('#footer-year').innerText = new Date().getFullYear();
