74209a76ebcf2ab95cf68a3e5ade333c6fb4de48
Ryan Bey <bey15002@byui.edu>
2022-03-22 15:45:11 -0600

    js spacing

--- js/subpage.js
+++ js/subpage.js
@@ -5,6 +5,5 @@ const jsonURL = 'https://raw.githubusercontent.com/ryanbey/just-pick-something/m
 // Build matchups using HTML I made for the first matchup
 buildMatchups(jsonURL);
 
-
 // Footer copyright date
 document.querySelector('#footer-year').innerText = new Date().getFullYear();
